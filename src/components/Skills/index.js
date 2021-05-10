e8764e9778e37d05e0f1ae363e38e9530edcc404
Vilmar Cabanero <vilmarcabanero@gmail.com>
2021-05-10 12:31:13 +0800

    Balik sa dati ang skills, di na gumamit map

--- src/components/Skills/index.js
+++ src/components/Skills/index.js
@@ -7,7 +7,7 @@ import {
 	SkillContainer,
 	Skill,
 } from './index.style'
-import { Container, Row } from 'react-bootstrap'
+import { Container, Row, Col } from 'react-bootstrap'
 import 'bootstrap/dist/css/bootstrap.css'
 import css3 from '../../assets/images/logo-css3.png'
 import javascript from '../../assets/images/logo-javascript.png'
@@ -26,103 +26,161 @@ export default function About() {
 	return (
 		<Container>
 			<MyContainer>
-				<Title>Skills</Title>
-				<Details>
-					I'm a full stack developer who uses the MERN Stack to develop my web
-					apps or websites. My current skill set is as follows:
-				</Details>
+				<Row>
+					<Title>Skills</Title>
+					<Details>
+						I'm a full stack developer who uses the MERN Stack to develop my web
+						apps or websites. My current skill set is as follows:
+					</Details>
 
-				<ImageContainer>
-					{skills.map(items => {
-						return (
-							<SkillContainer>
-								<Skill>{items.skill}</Skill>
-								<Image src={items.src} id={items.id} alt={items.alt} />
-							</SkillContainer>
-						)
-					})}
-					<SkillContainer>
-						<Skill>Gitlab CI/CD</Skill>
-						<Image src={gitlabcicd} id='gitlab-ci-cd' alt='gitlab-ci-cd' />
-					</SkillContainer>
-				</ImageContainer>
+					<ImageContainer>
+						<SkillContainer>
+							<Skill>CSS3</Skill>
+							<Image src={css3} alt='css3' />
+						</SkillContainer>
+
+						<SkillContainer>
+							<Skill>javascript</Skill>
+							<Image src={javascript} alt='javascript' />
+						</SkillContainer>
+
+						<SkillContainer>
+							<Skill>mongodb</Skill>
+							<Image src={mongodb} alt='mongodb' />
+						</SkillContainer>
+
+						<SkillContainer>
+							<Skill>nodejs</Skill>
+							<Image src={nodejs} alt='nodejs' />
+						</SkillContainer>
+
+						<SkillContainer>
+							<Skill>expressjs</Skill>
+							<Image src={expressjs} alt='expressjs' />
+						</SkillContainer>
+
+						<SkillContainer>
+							<Skill>react</Skill>
+							<Image src={react} alt='react' />
+						</SkillContainer>
+
+						<SkillContainer>
+							<Skill>heroku</Skill>
+							<Image src={heroku} alt='heroku' />
+						</SkillContainer>
+
+						<SkillContainer>
+							<Skill>git</Skill>
+							<Image src={git} alt='git' />
+						</SkillContainer>
+
+						<SkillContainer>
+							<Skill id='gitlab-ci-cd'>Gitlab CI/CD</Skill>
+							<Image src={gitlabcicd} alt='gitlabcicd' />
+						</SkillContainer>
+
+						<SkillContainer>
+							<Skill>rest</Skill>
+							<Image src={rest} alt='rest' />
+						</SkillContainer>
+
+						<SkillContainer>
+							<Skill>postman</Skill>
+							<Image src={postman} alt='postman' />
+						</SkillContainer>
+
+						<SkillContainer>
+							<Skill>bootstrap</Skill>
+							<Image src={bootstrap} alt='bootstrap' />
+						</SkillContainer>
+					</ImageContainer>
+				</Row>
 			</MyContainer>
 		</Container>
 	)
 }
 
-const skills = [
-	{
-		skill: 'css3',
-		src: css3,
-		alt: 'css3',
-		id: '',
-	},
-	{
-		skill: 'javascript',
-		src: javascript,
-		alt: 'javascript',
-		id: '',
-	},
+// const skills = [
+// 	{
+// 		skill: 'css3',
+// 		src: css3,
+// 		alt: 'css3',
+// 		id: '',
+// 	},
+// 	{
+// 		skill: 'javascript',
+// 		src: javascript,
+// 		alt: 'javascript',
+// 		id: '',
+// 	},
 
-	{
-		skill: 'mongodb',
-		src: mongodb,
-		alt: 'mongodb',
-		id: '',
-	},
-	{
-		skill: 'nodejs',
-		src: nodejs,
-		alt: 'nodejs',
-		id: '',
-	},
-	{
-		skill: 'expressjs',
-		src: expressjs,
-		alt: 'expressjs',
-		id: '',
-	},
-	{
-		skill: 'react',
-		src: react,
-		alt: 'react',
-		id: '',
-	},
-	{
-		skill: 'heroku',
-		src: heroku,
-		alt: 'heroku',
-		id: '',
-	},
-	{
-		skill: 'git',
-		src: git,
-		alt: 'git',
-		id: '',
-	},
-	// {
-	// 	skill: 'Gitlab CI/CD',
-	// 	src: gitlabcicd,
-	// 	alt: 'Gitlab CI/CD',
-	// 	id: 'gitlab-ci-cd'
-	// },
-	{
-		skill: 'rest',
-		src: rest,
-		alt: 'rest',
-		id: '',
-	},
-	{
-		skill: 'postman',
-		src: postman,
-		alt: 'postman',
-		id: '',
-	},
-	{
-		skill: 'bootstrap',
-		src: bootstrap,
-		alt: 'bootstrap',
-		id: '',
-	},
-]
+// 	{
+// 		skill: 'mongodb',
+// 		src: mongodb,
+// 		alt: 'mongodb',
+// 		id: '',
+// 	},
+// 	{
+// 		skill: 'nodejs',
+// 		src: nodejs,
+// 		alt: 'nodejs',
+// 		id: '',
+// 	},
+// 	{
+// 		skill: 'expressjs',
+// 		src: expressjs,
+// 		alt: 'expressjs',
+// 		id: '',
+// 	},
+// 	{
+// 		skill: 'react',
+// 		src: react,
+// 		alt: 'react',
+// 		id: '',
+// 	},
+// 	{
+// 		skill: 'heroku',
+// 		src: heroku,
+// 		alt: 'heroku',
+// 		id: '',
+// 	},
+// 	{
+// 		skill: 'git',
+// 		src: git,
+// 		alt: 'git',
+// 		id: '',
+// 	},
+// 	{
+// 		skill: 'Gitlab CI/CD',
+// 		src: gitlabcicd,
+// 		alt: 'Gitlab CI/CD',
+// 		id: 'gitlab-ci-cd'
+// 	},
+// 	{
+// 		skill: 'rest',
+// 		src: rest,
+// 		alt: 'rest',
+// 		id: '',
+// 	},
+// 	{
+// 		skill: 'postman',
+// 		src: postman,
+// 		alt: 'postman',
+// 		id: '',
+// 	},
+// 	{
+// 		skill: 'bootstrap',
+// 		src: bootstrap,
+// 		alt: 'bootstrap',
+// 		id: '',
+// 	},
+// ]
+
+// {skills.map(items => {
+//   return (
+//     <SkillContainer>
+//       <Skill>{items.skill}</Skill>
+//       <Image src={items.src} id={items.id} alt={items.alt} />
+//     </SkillContainer>
+//   )
+// })}
